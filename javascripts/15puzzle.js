96e11cc12ad9a25ebcb2a1d9bcccf06a33ecc472
david <dacastivel@outlook.com>
2018-12-10 19:49:59 -0800

    Rotate image

--- javascripts/15puzzle.js
+++ javascripts/15puzzle.js
@@ -5,19 +5,31 @@ Array.prototype.swap = function (i, j)
 	this[j] = tmp;
 }
 
+var totalRows = 4, totalColumns = 4;
 var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0];
 var pos = [15, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+var degrees = 0;
 
 // var num = [7, 12, 8, 2, 11, 1, 6, 3, 10, 5, 4, 0, 9, 13, 14, 15];
 // var pos = [11, 5, 3, 7, 10, 9, 6, 0, 2, 12, 8, 4, 1, 13, 14, 15];
 
 var totalWidth = 0, totalHeight = 0;
 
+function rotateCoord(r, c, degrees) {
+	if (degrees == 0) {
+		return {row: r, column: c};
+	} if (degrees == 90) {
+		return {row: totalColumns-1-c, column: r};
+	} else if (degrees == 180) {
+		return {row: totalRows-1-r, column: totalColumns-1-c};
+	} else {
+		return {row: c, column: totalRows-1-r};
+	}
+}
+
 // Degrees should be only 90 or 270
-function draw(image, degrees, imageRow, imageColumn, totalRows, totalColumns, puzzleRow, puzzleColumn, tileId) {
-    var canvasJQuery = $(document.createElement("canvas"));
-    var canvas = canvasJQuery[0];
-    var ctx = canvas.getContext("2d");
+function draw(image, degrees, imageRow, imageColumn, puzzleRow, puzzleColumn) {
+	var shouldSwapWidthAndHeight = degrees == 90 || degrees == 270;
 
     var maxTotalWidth = 648;
 	var canvasWidth = image.width > maxTotalWidth ? maxTotalWidth : image.width;
@@ -26,44 +38,50 @@ function draw(image, degrees, imageRow, imageColumn, totalRows, totalColumns, pu
 	canvasWidth /= totalColumns;
 	canvasHeight /= totalRows;
 
-    // always swap width and height
-    canvas.width = canvasHeight;
-    canvas.height = canvasWidth;
+	totalWidth += (shouldSwapWidthAndHeight ? canvasHeight : canvasWidth) / totalColumns;
+    totalHeight += (shouldSwapWidthAndHeight ? canvasWidth : canvasHeight) / totalRows;
 
-    totalWidth += canvasHeight / totalColumns;
-    totalHeight += canvasWidth / totalRows;
+    var tileId = true ? imageRow * totalColumns + imageColumn : imageColumn * totalRows + imageRow;
+    if (tileId == 15) {
+    	return;
+    }
 
+	var canvasJQuery = $(document.createElement("canvas"));
+	canvasJQuery.addClass("square");
+    canvasJQuery.attr("id", tileId+1);
+
+    // Don't know why you can't set javascript width/height with float values.
+    canvasJQuery.width(shouldSwapWidthAndHeight ? canvasHeight : canvasWidth);
+    canvasJQuery.height(shouldSwapWidthAndHeight ? canvasWidth : canvasHeight);
+
+    var canvas = canvasJQuery[0];
+    canvas.width = shouldSwapWidthAndHeight ? canvasHeight : canvasWidth;
+   	canvas.height = shouldSwapWidthAndHeight ? canvasWidth : canvasHeight;
+   	canvas.style.left = (true ? puzzleColumn : puzzleRow)*25 + "%";
+    canvas.style.top = (true ? puzzleRow : puzzleColumn)*25 + "%";
+    canvas.style.position = "absolute";
+
+	var ctx = canvas.getContext("2d");
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.translate(canvas.width/2, canvas.height/2);
     ctx.rotate(degrees*Math.PI/180);
 
-    // drawImage(imageObj, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
     var sourceWidth = image.width / totalColumns;
     var sourceHeight = image.height / totalRows;
-    var sourceX = sourceWidth * (totalRows - imageRow - 1);
-    var sourceY = sourceHeight * imageColumn;
+    var coordRotated = rotateCoord(imageRow, imageColumn, degrees);
+    var sourceX = sourceWidth * coordRotated.column;
+    var sourceY = sourceHeight * coordRotated.row;
+    var destX = shouldSwapWidthAndHeight ? -canvas.height/2 : -canvas.width/2;
+    var destY = shouldSwapWidthAndHeight ? -canvas.width/2 : -canvas.height/2;
+    var destWidth = shouldSwapWidthAndHeight ? canvas.height : canvas.width;
+    var destHeight = shouldSwapWidthAndHeight ? canvas.width : canvas.height;
 
-    var tileId = imageRow * totalColumns + imageColumn;
-    if (tileId != 15) {
-    	ctx.drawImage(image, sourceX, sourceY, sourceWidth, sourceHeight, -canvas.height/2, -canvas.width/2, canvas.height, canvas.width);
-    	document.getElementById('grid').appendChild(canvas);
-    }
-
-    canvas.style.left = puzzleColumn*25 + "%";
-    canvas.style.top = puzzleRow*25 + "%";
-    canvas.style.position = "absolute";
-
-    // Don't know why you can't set javascript width/height with float values.
-    canvasJQuery.width(canvasHeight);
-    canvasJQuery.height(canvasWidth);
-    canvasJQuery.addClass("square");
-    canvasJQuery.attr("id", tileId+1);
+	// drawImage(imageObj, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+	ctx.drawImage(image, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
+	document.getElementById('grid').appendChild(canvas);
 }
 
 $(document).ready(function(){
-
-	console.log("num:", num);
-	console.log("pos:", pos);
 
 	Init();
 
@@ -135,6 +153,11 @@ $(document).ready(function(){
 		Init();
 	});
 
+	$('#rotate').click(function() {
+		degrees = (degrees + 90) % 360;
+		Init();
+	});
+
 	// funciones
 
 	function autoSolve(sAns)
@@ -165,21 +188,21 @@ $(document).ready(function(){
 
 	function Init()
 	{
+		console.log("num:", num);
+		console.log("pos:", pos);
+
 		totalWidth = 0;
 		totalHeight = 0;
 		$('#grid').empty();
 		var img = new Image();
 		img.onload = function() {
-			// for (var i = 0; i < 4; ++i)
-			// 	for (var j = 0; j < 4; ++j)
-			// 		draw(img, 270, i, j, 4, 4, i == 3 && j == 3);
 			for (var i = 0; i < 16; ++i) {
 				var positionInImage = (num[i] == 0 ? 15 : num[i]-1);
 				var imageColumn = positionInImage % 4;
 				var imageRow = Math.floor(positionInImage/4);
-				var puzzleColumn = i % 4;
-				var puzzleRow = Math.floor(i/4);
-				draw(img, 270, imageRow, imageColumn, 4, 4, puzzleRow, puzzleColumn, num[i]);
+				var puzzleColumn = true ? pos[num[i]] % 4 :  Math.floor(i/4);
+				var puzzleRow = true ? Math.floor(pos[num[i]]/4) : i % 4;
+				draw(img, degrees, imageRow, imageColumn, puzzleRow, puzzleColumn);
 			}
 
 			$("#grid").width(totalWidth);
